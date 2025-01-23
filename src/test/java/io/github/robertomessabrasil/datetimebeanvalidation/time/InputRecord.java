8ed5d1569240517ac63bd7cfdf228f108d498bb4
Roberto Messa <wardog1@gmail.com>
2025-01-22 21:53:57 -0300

    feat: initial commit

--- src/test/java/io/github/robertomessabrasil/datetimebeanvalidation/time/InputRecord.java
+++ src/test/java/io/github/robertomessabrasil/datetimebeanvalidation/time/InputRecord.java
@@ -1,8 +1,6 @@
 package io.github.robertomessabrasil.datetimebeanvalidation.time;
 
 
-import io.github.robertomessabrasil.datetimebeanvalidation.datetime.ValidDateTime;
-
 public record InputRecord(
         @ValidTime(template = "HH:mm")
         String time
